a40e0d373264e3d8883ea22fe55b21ab7eff8e9d
Eugene Draitsev <ddrrai@gmail.com>
2021-01-22 20:47:28 +0100

    Added 'websockets' example

--- src/TestContext.js
+++ src/TestContext.js
@@ -1,4 +1,4 @@
-import React, { useState, memo } from 'react'
+import React, { useState, useEffect, memo } from 'react'
 
 const TestContext = React.createContext({ someValue: null })
 
@@ -6,6 +6,18 @@ const TestContext = React.createContext({ someValue: null })
 const TestProvider = memo(({ children }) => {
   // Initial value
   const [someValue] = useState(Math.random())
+
+  useEffect(() => {
+    // simulate websocket connection
+    const interval = setInterval(() => {
+      console.log(`Websocket messages from context ${someValue}`)
+    }, 1000)
+
+    return () => {
+      console.log(`Clear interval for context ${someValue}`)
+      clearInterval(interval)
+    }
+  }, [])
 
   return (
     <TestContext.Provider value={{ someValue }}>
